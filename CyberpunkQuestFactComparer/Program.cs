fde37bf4befe6a5a93c8014260f4d304e295cced
speerian <mail.ian.speer@gmail.com>
2021-12-11 02:58:55 -0500

    Added CyberpunkQuestFactComparer program.

---
+++ CyberpunkQuestFactComparer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace CyberpunkQuestFactComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factsDirectory = args.Length > 0 ? args[0] : @"C:\Users\Ian\Documents\Code\quick_hack_breach_protocol_bug\facts";
+            var successfulFacts = ParseFacts(factsDirectory, "SuccessfulSaveQuestFacts.txt");
+            var buggedFacts = ParseFacts(factsDirectory, "BuggedSaveQuestFacts.txt");
+
+            var identicalFacts = buggedFacts.ToList().Intersect(successfulFacts.ToList());          
+            Console.WriteLine($"Found {identicalFacts.Count()} identical facts.");
+            ExportFacts(factsDirectory, "identicalfacts.json", identicalFacts);
+            
+            var factsWithDifferingValues = buggedFacts.Intersect(successfulFacts, new PartialQFEqualityComparer());
+            Console.WriteLine($"Found {factsWithDifferingValues.Count()} facts with differing values.");
+            ExportFacts(factsDirectory, "factsWithDifferingValues.json", factsWithDifferingValues);
+
+            var uniqueBugged = buggedFacts.Except(identicalFacts).Except(factsWithDifferingValues);
+            Console.WriteLine($"{uniqueBugged.Count()} unique facts from bugged playthrough");
+            ExportFacts(factsDirectory, "uniqueBuggedFacts.json", uniqueBugged);
+
+            var uniqueSuccessful = successfulFacts.Except(identicalFacts).Except(factsWithDifferingValues);
+            Console.WriteLine($"{uniqueSuccessful.Count()} unique facts from successful playthrough");
+            ExportFacts(factsDirectory, "uniqueSuccessfulFacts.json", uniqueSuccessful);
+        }
+
+        private static void ExportFacts(string directory, string fileName, IEnumerable<QuestFact> facts)
+        {
+            var fileLocation = Path.Combine(directory, fileName);
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(facts, options);
+            File.WriteAllText(fileLocation, jsonString);
+            Console.WriteLine($"Exported {fileName}\n");
+        }
+
+        private static IEnumerable<QuestFact> ParseFacts(string directory, string fileName)
+        {
+            var fileLocation = Path.Combine(directory, fileName);
+            var jsonText = File.ReadAllText(fileLocation);
+            return JsonSerializer.Deserialize<List<QuestFact>>(jsonText);
+        }
+    }
+}
---
+++ CyberpunkQuestFactComparer/QuestFact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CyberpunkQuestFactComparer
+{
+    public class QuestFact : IEquatable<QuestFact>
+    {
+        public long Hash { get; set; }
+        public string FactName { get; set; }
+        public int Value { get; set; }
+
+        public bool Equals([AllowNull] QuestFact other)
+        {
+            return Hash == other.Hash && 
+                   FactName == other.FactName && 
+                   Value == other.Value;
+        }
+
+        public override bool Equals(object obj) => Equals(obj as QuestFact);
+        public override int GetHashCode() => (Hash, FactName, Value).GetHashCode();
+    }
+
+    public class PartialQFEqualityComparer : IEqualityComparer<QuestFact>
+    {
+        public bool Equals([AllowNull] QuestFact x, [AllowNull] QuestFact y) => x.Hash == y.Hash && x.FactName == y.FactName && x.Value != y.Value;
+        public int GetHashCode([DisallowNull] QuestFact obj) => (obj.Hash, obj.FactName).GetHashCode();
+    }
+}
